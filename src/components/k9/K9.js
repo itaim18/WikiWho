8b2491cc828213063803f7d094bd2d914f946417
itaim18 <37772742+itaim18@users.noreply.github.com>
2022-09-20 15:05:13 +0300

    add volume icon

--- src/components/k9/K9.js
+++ src/components/k9/K9.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 import styles from "./k9.module.css";
 import audio from "../../assets/sounds/yes.mpeg";
+import { BsFillVolumeMuteFill, BsFillVolumeUpFill } from "react-icons/bs";
 const K9 = () => {
   const [src, setSrc] = useState("images/k9-dog.png");
-
+  const [isSound, setIsSound] = useState(false);
   const startAnimation = () => {
     setTimeout(() => {
       setSrc("images/k9-runs.png");
@@ -17,7 +18,11 @@ const K9 = () => {
   const linkToVid = (event) => {
     switch (event.detail) {
       case 1: {
+        setIsSound(true);
         new Audio(audio).play();
+        setTimeout(() => {
+          setIsSound(false);
+        }, 2000);
         break;
       }
 
@@ -41,6 +46,17 @@ const K9 = () => {
       onMouseLeave={stopAnimation}
       onClick={linkToVid}
     >
+      <h2
+        style={{
+          textAlign: "right",
+          position: "relative",
+          top: "40px",
+          color: "white",
+        }}
+      >
+        {isSound && <BsFillVolumeUpFill />}
+        {!isSound && <BsFillVolumeMuteFill />}
+      </h2>
       <img src={src} alt="doge" className={styles.pageIcon} />
     </div>
   );
